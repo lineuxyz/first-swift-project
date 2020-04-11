e231bd3850ca675a05c1cd5066ad1724fca4686a
Lineu Pastorelli <lineualberto@gmail.com>
2020-04-11 19:31:13 -0300

    Initial Commit

---
+++ twitter-clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  twitter-clone
+//
+//  Created by Lineu Alberto on 11/04/20.
+//  Copyright © 2020 Lineu Alberto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
