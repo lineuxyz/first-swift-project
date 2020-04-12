340a35b1e4ebb35139f14723dc0247f76ee06fb3
Lineu Pastorelli <lineualberto@gmail.com>
2020-04-12 20:00:23 -0300

    uppload main view

--- twitter-clone/ContentView.swift
+++ twitter-clone/ContentView.swift
@@ -10,7 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+            NavigationView {
+                HomeView()
+                .navigationBarTitle(Text("Main"), displayMode: .inline)
+            }.tabItem {
+                Image("home")
+                Text("Home")
+            }
+                Text("Search")
+                .tabItem {
+                    Image("search")
+                    Text("Search")
+            }
+                
+            Text("Notifications")
+                .tabItem {
+                    Image("notification")
+                    Text("Notifications")
+            }
+                
+            Text("Messages")
+                .tabItem {
+                    Image("letter")
+                    Text("Messages")
+            }
+        }.edgesIgnoringSafeArea(.top)
     }
 }
 
@@ -18,4 +43,32 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct HomeView: View {
+    let tweets: [Tweet] = tweetdata
+    var body: some View {
+        List {
+            ForEach(tweets) { tweet in
+                VStack {
+                    Image("profile1")
+                    
+                    VStack {
+                        HStack {
+                            Text("ddsada")
+                            Text("ddsada")
+                        }
+                        Text("dasdsadas")
+                        
+                        HStack {
+                            Image("comment")
+                            Image("retweet")
+                            Image("heart")
+                            Image("share")
+                        }
+                    }
+                }
+        }
+    }
+}
 }
