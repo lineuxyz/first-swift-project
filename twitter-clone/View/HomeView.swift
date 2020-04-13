0d1f753ba1ca4db074f6d18ec166aef987faaadc
Lineu Pastorelli <lineualberto@gmail.com>
2020-04-13 18:29:09 -0300

    uppload config do projetos

---
+++ twitter-clone/View/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  twitter-clone
+//
+//  Created by Lineu Alberto on 13/04/20.
+//  Copyright © 2020 Lineu Alberto. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
